7c0a1a2dc9b41b723ec04fb9ef6856f84bb7b95d
BuildTools <xnaxdy@hydra-development.net>
2018-05-03 21:06:38 +0100

    things

--- ShipStatus.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * Represents the status of a ship or one square of a ship
- */
-public enum ShipStatus
-{
-    NONE, INTACT, HIT, SUNK
-}
---
+++ ShotStatus.java
@@ -0,0 +1,8 @@
+
+
+
+public enum ShotStatus
+{
+	HIT, MISS, SINK
+
+}
