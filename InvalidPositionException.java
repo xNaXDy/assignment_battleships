9730560280eb47507747a11f229ab478ee9f4b3e
BuildTools <xnaxdy@hydra-development.net>
2018-05-03 21:06:47 +0100

    more things

---
+++ InvalidPositionException.java
@@ -0,0 +1,21 @@
+
+
+public class InvalidPositionException extends Exception
+{
+
+    public InvalidPositionException()
+    {
+    }
+
+    public InvalidPositionException(String message){
+        super(message);
+    }
+
+    public InvalidPositionException(Throwable cause){
+        super(cause);
+    }
+    
+    public InvalidPositionException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
---
+++ ShipOverlapException.java
@@ -0,0 +1,20 @@
+
+
+public class ShipOverlapException extends Exception
+{
+    public ShipOverlapException()
+    {
+    }
+
+    public ShipOverlapException(String message){
+        super(message);
+    }
+
+    public ShipOverlapException(Throwable cause){
+        super(cause);
+    }
+    
+    public ShipOverlapException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
